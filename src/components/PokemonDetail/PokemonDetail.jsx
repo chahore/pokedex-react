a7c76f8b11327eafe9f0c9aede764fb4d2fae70f
davidczachor <david@czachor.dev>
2023-05-04 20:43:40 -0400

    Added modal for pokemon detail

--- src/components/PokemonDetail/PokemonDetail.jsx
+++ src/components/PokemonDetail/PokemonDetail.jsx
@@ -1,7 +1,31 @@
+import Modal from "react-modal";
+import { useState, useEffect } from "react";
 import "./PokemonDetail.css";
 
 function PokemonDetail({ pokemon }) {
-  return <div>{console.log(pokemon.name)}</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    setIsOpen(!!pokemon);
+  }, [pokemon]);
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={closeModal}
+      className="pokemon-detail"
+      overlayClassName="Overlay"
+    >
+      <h2>{pokemon.name}</h2>
+      <img src={pokemon.imageUrl} alt={pokemon.name} />
+      <p>{pokemon.description}</p>
+      <button onClick={closeModal}>Close</button>
+    </Modal>
+  );
 }
 
 export default PokemonDetail;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import Modal from "react-modal";
 import "./index.css";
+
+Modal.setAppElement("#root");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
